ddc6c8af9c1d2c82e46c1a9e1b0a9597a4052d7d
FelipeSantana <felipesantana27@gmail.com>
2024-10-23 21:50:57 -0300

    creating methods and logic of the program

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,33 @@
+import entities.Product;
+
 import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+
         Locale.setDefault(Locale.US);
         Scanner scanner = new Scanner(System.in);
 
-
+        System.out.println("Enter the quantity of products ");
         int n = scanner.nextInt();
+        Product[] vect = new Product[n];
 
+        for (int i = 0; i < n; i++) {
+            System.out.println("Enter the product name ");
+            String name = scanner.next();
 
+            System.out.println("Enter the product price ");
+            double price = scanner.nextDouble();
+            vect[i] = new Product(name, price);
+        }
+        double sum = 0.0;
+        for (int i = 0; i < n; i++) {
+            sum += vect[i].getPrice();
+        }
+        double avg = sum / n;
+        System.out.printf("Average price: %.2f%n", avg);
 
     }
 }
